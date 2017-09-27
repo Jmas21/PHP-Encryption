8a9c13366cbf7ca0ab41fa966addaeb474d3de9c
Jmas21 <natalycamachof@gmail.com>
2017-09-27 09:19:20 -0500

    Added Encryption php

---
+++ Encryption.php
@@ -0,0 +1,23 @@
+<?php
+    $pass = "test";
+    $salt = "database";
+    echo $pass. "\n";
+    echo "MD5: " . md5($pass). "\n";
+    echo "crypt: " . crypt($pass). "\n";
+    echo "crypt+salt: ". crypt($pass, $salt)."\n";
+    echo "crypt+ md5: ". crypt($pass, md5($salt))."\n";
+    echo "SHA: ". sha1($pass)."\n";
+    echo "HASH: ". password_hash($pass, PASSWORD_DEFAULT)."\n";
+    
+    echo "PASSWORD VERIFY: \n";
+    $ohgod = "test";
+    $whygod = "test";
+    $pas  = password_hash($whygod, PASSWORD_DEFAULT);
+    $hash = '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq';
+    if (password_verify($ohgod, $pas)) {
+           echo 'Password is valid!';
+    } else {
+           echo 'Invalid password.';
+    }
+
+?>
